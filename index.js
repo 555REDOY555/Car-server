f6e50d59acef66c448fa0c4c60c48176e1d58716
unknown <555REDOY555>
2021-11-11 18:36:23 +0600

    go

--- index.js
+++ index.js
@@ -29,21 +29,21 @@ admin.initializeApp({
 app.use(cors());
 app.use(express.json());
 
-// async function verifyToken(req, res, next) {
-//      if (req.headers?.authorization?.startsWith('Bearer ')) {
-//           const token = req.headers.authorization.split(' ')[1];
+async function verifyToken(req, res, next) {
+     if (req.headers?.authorization?.startsWith('Bearer ')) {
+          const token = req.headers.authorization.split(' ')[1];
 
-//           try {
-//                const decodedUser = await admin.auth().verifyIdToken(token);
-//                req.decodedEmail = decodedUser.email;
-//           }
-//           catch {
+          try {
+               const decodedUser = await admin.auth().verifyIdToken(token);
+               req.decodedEmail = decodedUser.email;
+          }
+          catch {
 
-//           }
+          }
 
-//      }
-//      next();
-// }
+     }
+     next();
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.to6vq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -142,6 +142,15 @@ async function run() {
                          res.send(result);
                     });
           });
+          app.delete("/Cars/:id", async (req, res) => {
+               console.log(req.params.id);
+
+               CarsCllection
+                    .deleteOne({ _id: ObjectId(req.params.id) })
+                    .then((result) => {
+                         res.send(result);
+                    });
+          });
 
 
           // POST API 
@@ -183,7 +192,7 @@ async function run() {
 
 
           //make a admin
-          app.put('/users/admin', async (req, res) => {
+          app.put('/users/admin', verifyToken, async (req, res) => {
                const user = req.body;
                const requester = req.decodedEmail;
                if (requester) {
@@ -193,13 +202,15 @@ async function run() {
                          const updateDoc = { $set: { role: 'admin' } };
                          const result = await UserCollection.updateOne(filter, updateDoc);
                          res.json(result);
+                         console.log(result)
                     }
                }
                else {
-                    res.status(403).json({ message: 'You are not allowed here' })
+                    res.status(403).json({ message: 'you do not have access to make admin' })
                }
 
           })
+
 
 
      }
